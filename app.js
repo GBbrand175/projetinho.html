2c41e5b3b0bd70edc16b192df7fe51f94a38ced7
GBbrand175 <95517895+GBbrand175@users.noreply.github.com>
2022-03-15 04:17:18 -0300

    alteração novo botao na minha page

---
+++ app.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("botao1");
+
+btn.addEventListener("click", function(){
+    window.location.href ="https://www.ifood.com.br/delivery/maceio-al/smash-parrilla-jatiuca/6e5177f9-93e6-464b-b36a-0352447ca15a"
+})
